691bad55a02d27f5491da98198c2546eaa9b6449
Prabha8859 <prabha8859@gmail.com>
2025-12-30 13:03:57 +0530

    only formate app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import AppRouter from './routes/approuter.jsx';
-import "./App.css"
+import React from "react";
+import AppRouter from "./routes/approuter.jsx";
+import "./App.css";
 
 function App() {
   return (
-    <div className=''>
+    <div className="">
       <AppRouter />
     </div>
-  )
-
+  );
 }
 export default App;
