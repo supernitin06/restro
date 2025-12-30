b129399bd7169cd1a9622dec52d1a8ce38ce89c7
Nitin Chauhan <147541211+supernitin06@users.noreply.github.com>
2025-12-30 12:48:12 +0530

    dashboard

--- restro/src/outlet/outlet.jsx
+++
@@ -1,9 +0,0 @@
-import outlet from "react-router-dom";
-
-import React from 'react'
-
-export const outlet = () => {
-  return (
-    <div>outlet</div>
-  )
-}
--- restro/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  darkMode: 'class',
-  content: ['./index.html', './src/**/*.{js,jsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
