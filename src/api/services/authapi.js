0661a8d3c8e68010ebf100b40178dbab0613a731
Nitin Chauhan <147541211+supernitin06@users.noreply.github.com>
2026-01-05 18:51:40 +0530

    latest

--- src/api/services/authapi.js
+++ src/api/services/authapi.js
@@ -1,19 +1,20 @@
 import { baseApi } from "../services/baseApi";
+import { setCredentials } from "./authSlice";
 
 export const authApi = baseApi.injectEndpoints({
-    endpoints: (builder) => ({
+  endpoints: (builder) => ({
 
-        // 🔹 LOGIN
-        login: builder.mutation({
-            query: (payload) => ({
-                url: "admin/login",
-                method: "POST",
-                data: payload,
-            }),
+    // 🔹 LOGIN
+    login: builder.mutation({
+      query: (payload) => ({
+        url: "admin/login",
+        method: "POST",
+        data: payload,
+      }),
 
-            async onQueryStarted(arg, { dispatch, queryFulfilled }) {
-                try {
-                    const { data } = await queryFulfilled;
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+        try {
+          const { data } = await queryFulfilled;
 
           // Dispatch setCredentials to update auth state in Redux and localStorage
           dispatch(setCredentials({ token: data.token, user: data.user }));
